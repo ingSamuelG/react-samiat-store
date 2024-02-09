008a67745a0c755f2224733f76eff59f96b23193
Samuel Gomez <samunefalara80@gmail.com>
2024-02-09 13:16:47 -0400

    feat: first commit of the alert component

--- src/routes/layout/layout.component.jsx
+++ src/routes/layout/layout.component.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Fragment } from "react";
 import NavBar from "../../components/nav-bar/nav-bar.component";
+import AlertContainer from "../../components/alert-container/alert-container.component";
 
 const Layout = () => {
   return (
     <Fragment>
       <NavBar />
+      {/* <AlertContainer /> */}
       <Outlet />
     </Fragment>
   );
