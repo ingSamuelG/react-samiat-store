da168cc76ccdcb70a4df4c755688f464b8b7c446
Samuel Gomez <samunefalara80@gmail.com>
2024-03-02 17:32:04 -0400

    feat:add redirects file for netlify and add routes in the index

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -3,12 +3,19 @@ import {
   BackgroundImage,
   DirectoryBody,
 } from "./directory-item.style.jsx";
+import { useNavigate } from "react-router-dom";
 
 const DirectoryItem = ({ category: { _, title, imageUrl } }) => {
+  const navigate = useNavigate();
+
+  const handleShopButtonLink = () => {
+    navigate(`/shop/${title}`);
+  };
+
   return (
     <DirectoryItemContainer>
       <BackgroundImage imgUrl={imageUrl} />
-      <DirectoryBody>
+      <DirectoryBody onClick={handleShopButtonLink}>
         <h2>{title}</h2>
         <p>Shop Now</p>
       </DirectoryBody>
