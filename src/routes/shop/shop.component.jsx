ec186002a913e3c099e325739a3f69316fede330
Samuel Gomez <samunefalara80@gmail.com>
2024-03-22 10:42:51 -0400

    feat: use a Asyc context mngr

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,20 +3,14 @@ import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.componet";
 import Category from "../category/category.component";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.util";
-import { setCategoriesMap } from "../../store/categories/categories.actions";
+import { fetchCategoriesAsync } from "../../store/categories/categories.actions";
 
 function Shop() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategoriesMap(categoriesArray));
-    };
-
-    getCategoriesMap();
-  }, []);
+    dispatch(fetchCategoriesAsync());
+  }, [dispatch]);
 
   return (
     <Routes>
