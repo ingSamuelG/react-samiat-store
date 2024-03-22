1cf8df49e6fd03de3aa6ad6ff3f5793c2c009f53
Samuel Gomez <samunefalara80@gmail.com>
2024-03-22 10:40:36 -0400

    feat: add spinner component for the is loading in categories, and some code to use thunk

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -3,32 +3,43 @@ import { useParams } from "react-router-dom";
 import { CategoryContainer, CategoryTitle } from "./category.styyle.jsx";
 import ProductCard from "../../components/product-card/product-card.component";
 import { useSelector } from "react-redux";
-import { selectCategoriesMap } from "../../store/categories/categories.selector.js";
+import Spinner from "../../components/spinner/spinner.component.jsx";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/categories.selector.js";
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [productsByCategory, setProductsByCategory] = useState(
     categoriesMap[category]
   );
 
   return (
     <>
-      <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-      <CategoryContainer>
-        {productsByCategory &&
-          productsByCategory.map(({ id, name, imageUrl, price }) => {
-            return (
-              <ProductCard
-                id={id}
-                title={name}
-                thumbnail={imageUrl}
-                price={price}
-                key={id}
-              />
-            );
-          })}
-      </CategoryContainer>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+          <CategoryContainer>
+            {productsByCategory &&
+              productsByCategory.map(({ id, name, imageUrl, price }) => {
+                return (
+                  <ProductCard
+                    id={id}
+                    title={name}
+                    thumbnail={imageUrl}
+                    price={price}
+                    key={id}
+                  />
+                );
+              })}
+          </CategoryContainer>
+        </>
+      )}
     </>
   );
 };
