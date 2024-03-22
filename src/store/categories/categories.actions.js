1bef6b5f087ceee78c90aad8a5f20aefdbd6a3b3
Samuel Gomez <samunefalara80@gmail.com>
2024-03-22 10:41:49 -0400

    feat: Create the logic to use a Asycn state manager

--- src/store/categories/categories.actions.js
+++ src/store/categories/categories.actions.js
@@ -1,6 +1,27 @@
 import { CATEGORIES_ACTION_TYPES } from "./categories.type";
 import { createAction } from "../../utils/reducer/reducer.utils";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.util";
 
-export const setCategoriesMap = (categoriesArray) => {
-  return createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesArray) => {
+  return createAction(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+};
+
+export const fetchCategoriesFailed = (error) => {
+  return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAIL, error);
+};
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart());
+  try {
+    const categoriesArray = await getCategoriesAndDocuments();
+    dispatch(fetchCategoriesSuccess(categoriesArray));
+  } catch (error) {
+    dispatch(fetchCategoriesFailed(error));
+  }
 };
