f78a43c22755dd622f2c0b35ab5351e7da9cca69
Samuel Gomez <samunefalara80@gmail.com>
2024-04-05 12:11:13 -0400

    refact: Pass categories to redux  toolkit

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,15 +1,20 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.componet";
 import Category from "../category/category.component";
-import { fetchCategoriesStart } from "../../store/categories/categories.actions";
+import { setCategories } from "../../store/categories/categories.reducer";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.util";
+import { useDispatch } from "react-redux";
 
 function Shop() {
   const dispatch = useDispatch();
-
   useEffect(() => {
-    dispatch(fetchCategoriesStart());
+    const fetchCategories = async () => {
+      const categories = await getCategoriesAndDocuments();
+      dispatch(setCategories(categories));
+    };
+
+    fetchCategories();
   }, [dispatch]);
 
   return (
